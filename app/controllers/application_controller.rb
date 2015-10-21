86e680d6d61198f2466c697e7881e81000f37f91
jetsekoopmans <jetsekoopmans@Jetses-MacBook-Air.local>
2015-10-21 11:34:55 +0200

    Init

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  require 'open-uri'
+
+
+  # def index
+  #   @restaus = [{"name"=>"Gaston Pure Food", "location"=>"Enschede, De Bothoven", "timeslots"=>["17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30", "21:45", "22:00"]}, {"name"=>"De Buurman - Restaurant de Dragonder", "location"=>"Hengelo, Centrum", "timeslots"=>["17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30"]}, {"name"=>"Het Seminar", "location"=>"Zenderen, Borne", "timeslots"=>["17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30"]}, {"name"=>"Brasserie de Beyaardier", "location"=>"Rijssen, Kern Rand Oost", "timeslots"=>["17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00"]}, {"name"=>"Restaurant-Pizzeria Sorrentino", "location"=>"Enschede, De Bothoven", "timeslots"=>["17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30", "21:45", "22:00"]}, {"name"=>"Landgoed het Rheins", "location"=>"Enter, Enter Zuid-West", "timeslots"=>["17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00"]}, {"name"=>"Savenije", "location"=>"Enschede, Buurtschap Lonneker-West", "timeslots"=>["17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00"]}]
+  # end
+end
