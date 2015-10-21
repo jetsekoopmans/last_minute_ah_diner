c630d33e7269b87e24c1c5695eeb013bfd84a58f
jetsekoopmans <jetsekoopmans@Jetses-MacBook-Air.local>
2015-10-21 14:36:22 +0200

    fonts

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module LastMinuteAhDiner
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join("app", "assets", "fonts", "codropsicons")
+    config.assets.paths << Rails.root.join("app", "assets", "fonts", "nlicons")
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
