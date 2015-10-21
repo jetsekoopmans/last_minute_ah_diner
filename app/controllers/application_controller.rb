f80f583f38bb09fb317745672723eb4ccf30c15f
jetsekoopmans <jetsekoopmans@Jetses-MacBook-Air.local>
2015-10-21 14:18:36 +0200

    Form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,13 @@ class ApplicationController < ActionController::Base
 
   def index
 
-    city = 'http://www.ahrestaurantactie.nl/search/Default.aspx?fh_location=//seatme/nl_NL/date%3E{20151021;30000101}/num_persons%3E{a2}/mam%3E{mam_1110;mam_1140;mam_1170;mam_1200;mam_1230;mam_1260;mam__1}/rid%3E{3062;2642;2643;2815;2838;3644;2843;4301;3369;4060;4216;4172;4177;4281;4008;3759;2293;3958;4157;3371;4262;2881;2839;4360;2842;2844;2836;2231;4288;2814;4148;4175;2875;2816;2813;2835;4052;4491;4389;4399;4425;4533;4540;4558;3376;4644;4665;4632;4708}'
+  end
+
+  def show
+    binding.pry
+    location = params['location']
+    persons = params['persons']
+    city = 'http://www.ahrestaurantactie.nl/direct_reserveren/zoek_restaurants.htm?fh_start_index=0&fh_location=' + location + '/num_persons%3E{a' + persons + '}'
     @restaus = []
 
     # Fetch and parse the city
@@ -100,7 +106,8 @@ class ApplicationController < ActionController::Base
       end
     end
   end
-  # def index
+
+  # def show
   #   @restaus = [{"name"=>"Gaston Pure Food", "location"=>"Enschede, De Bothoven", "timeslots"=>["17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30", "21:45", "22:00"]}, {"name"=>"De Buurman - Restaurant de Dragonder", "location"=>"Hengelo, Centrum", "timeslots"=>["17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30"]}, {"name"=>"Het Seminar", "location"=>"Zenderen, Borne", "timeslots"=>["17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30"]}, {"name"=>"Brasserie de Beyaardier", "location"=>"Rijssen, Kern Rand Oost", "timeslots"=>["17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00"]}, {"name"=>"Restaurant-Pizzeria Sorrentino", "location"=>"Enschede, De Bothoven", "timeslots"=>["17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30", "21:45", "22:00"]}, {"name"=>"Landgoed het Rheins", "location"=>"Enter, Enter Zuid-West", "timeslots"=>["17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00"]}, {"name"=>"Savenije", "location"=>"Enschede, Buurtschap Lonneker-West", "timeslots"=>["17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00"]}]
   # end
 end
